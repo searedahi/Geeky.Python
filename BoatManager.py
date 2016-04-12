0956a2232d2da145c1d604e4ec3e5d4b03595da0
searedahi <sborbust@gmail.com>
2016-04-11 18:11:29 -0700

    Classified

--- BoatManager.py
+++ BoatManager.py
@@ -1,21 +1,14 @@
 import time
-
-import DataOps
 import ThermometerService
-import TrollerService
-import LocatorService
 
 
-class BoatManager(object):
-    """this is the master task that calls into the other python libs"""
+print('hello')
 
+Tempy = ThermometerService.ThermometerService()
+print('starting')
+Tempy.runService()
 
-    def __init__(self, **kwargs):
-        thermoSvc = ThermometerService()
-
-        while True:
-            print(thermoSvc.CurrentFarenheight())
-            print('hello')
-            time.sleep(15)
-            pass
-
+while True:
+    print('loop')   
+    time.sleep(15)
+    pass
--- ThermometerService.py
+++ ThermometerService.py
@@ -4,44 +4,49 @@ import time
 import DataOps
 import ptvsd
 
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
+class ThermometerService(object):
+    """all the data operatiosn to my pi"""
 
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+    os.system('modprobe w1-gpio')
+    os.system('modprobe w1-therm')
 
-thermoData = DataOps.DataOps()
+    base_dir = '/sys/bus/w1/devices/'
+    device_folder = glob.glob(base_dir + '28*')[0]
+    device_file = device_folder + '/w1_slave'
 
+    thermoData = DataOps.DataOps()
+    lastTemperature = 0
 
-def read_temp_raw():
-	f = open(device_file, 'r')
-	lines = f.readlines()
-	f.close()
-	return lines
+    def read_temp_raw(self):
+        f = open(self.device_file, 'r')
+        lines = f.readlines()
+        f.close()
+        return lines
 
-def read_temp():
-	lines = read_temp_raw()
-	while lines[0].strip()[-3:] != 'YES':
-		time.sleep(0.2)
-		lines = read_temp_raw()
-	equals_pos = lines[1].find('t=')
-	if equals_pos != -1:
-		temp_string = lines[1][equals_pos+2:]
-		return temp_string
+    def read_temp(self):
+        lines = self.read_temp_raw()
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = read_temp_raw()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            return temp_string
 
+    def CurrentCelcius(self):
+        temp_string = lastTemperature
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
 
-def CurrentCelcius():
-    temp_string = read_temp()
-    temp_c = float(temp_string) / 1000.0
-    return temp_c
+    def CurrentFarenheight(self):
+        temp_c = CurrentCelcius()
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_f
 
-def CurrentFarenheight():
-    temp_c = CurrentCelcius()
-    temp_f = temp_c * 9.0 / 5.0 + 32.0
-    return temp_f
-
-while True:
-    rawTemp = read_temp()
-    thermoData.saveTemperature(rawTemp)
-    time.sleep(15)
+    def runService(self):
+        while True:
+            rawTemp = self.read_temp()
+            if rawTemp != self.lastTemperature:
+                self.thermoData.saveTemperature(rawTemp)
+                self.lastTemperature = rawTemp
+            time.sleep(6)

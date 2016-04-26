37aca5f121a1806b33653e97f9f6db1df61d592e
searedahi <sborbust@gmail.com>
2016-04-25 18:02:11 -0700

    working list of users. getting readt  to start hoking in the DAL

--- geekySeaApi.py
+++ geekySeaApi.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, jsonify
 import datetime
+import RPi.GPIO as GPIO
+
+
 app = Flask(__name__)
+GPIO.setmode(GPIO.BCM)
+
 
 @app.route("/")
 def hello():
@@ -11,7 +16,6 @@ def hello():
       'time': timeString
       }
    return render_template('GeekyLanding.html', **templateData)
-
 
 
 @app.route("/readPin/<pin>")
@@ -33,7 +37,79 @@ def readPin(pin):
    return render_template('PinStatus.html', **templateData)
 
 
+@app.route('/messages', methods = ['POST'])
+def api_message():
 
+    if request.headers['Content-Type'] == 'text/plain':
+        return "Text Message: " + request.data
+
+    elif request.headers['Content-Type'] == 'application/json':
+        return "JSON Message: " + json.dumps(request.json)
+
+    else:
+        return "415 Unsupported Media Type ;)"
+
+
+@app.route('/echo', methods = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def api_echo():
+    if request.method == 'GET':
+        return "ECHO: GET\n"
+
+    elif request.method == 'POST':
+        return "ECHO: POST\n"
+
+    elif request.method == 'PATCH':
+        return "ECHO: PACTH\n"
+
+    elif request.method == 'PUT':
+        return "ECHO: PUT\n"
+
+    elif request.method == 'DELETE':
+        return "ECHO: DELETE"
+
+
+
+class SimpleUser(object):
+    Id = -1
+    FirstName = ''
+    LastName = ''
+    
+    def serialize(self):
+        return {
+            'Id': self.Id, 
+            'FirstName': self.FirstName,
+            'LastName': self.LastName,
+        }
+    pass
+
+@app.route('/users/<userid>', methods = ['GET'])
+def api_users(userid):
+
+    usersList = []
+    for count in range(30):
+        x = SimpleUser()
+        x.Id = count
+        x.FirstName = 'John'
+        x.LastName = 'Somename'
+        usersList.append(x)
+    
+    rsp = jsonify(usrs=[e.serialize() for e in usersList])
+    
+    return rsp
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+            'status': 404,
+            'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
+app.error_handler_spec[None][404] = not_found
 
 
 if __name__ == "__main__":
